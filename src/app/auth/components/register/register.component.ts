e88508325513f3f659aef67bc296b7086740a955
KawalaE <edyta.wer.kawala@gmail.com>
2025-03-08 16:07:49 +0100

    feat: bind form with the FormBuilder form

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+interface RegistrationForm {
+  username: string;
+  email: string;
+  password: string;
+}
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
+  imports: [FormsModule, ReactiveFormsModule],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  fb = inject(FormBuilder);
+
+  signUpForm = this.fb.nonNullable.group({
+    username: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  public onSubmit(): void {
+    console.log('form', this.signUpForm.getRawValue());
+  }
+}
