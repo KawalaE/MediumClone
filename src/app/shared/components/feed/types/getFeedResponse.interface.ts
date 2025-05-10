56845939e6ea01ef8216dc0cf7af717dc5995149
KawalaE <edyta.wer.kawala@gmail.com>
2025-05-10 18:23:35 +0200

    feat: create get feed method in feed service

---
+++ src/app/shared/components/feed/types/getFeedResponse.interface.ts
@@ -0,0 +1,6 @@
+import { ArticleInterface } from '../../../types/articleInterface.interface';
+
+export interface GetFeedResponseInterface {
+  articles: ArticleInterface;
+  articlesCount: number;
+}
---
+++ src/app/shared/types/articleInterface.interface.ts
@@ -0,0 +1,12 @@
+export interface ArticleInterface {
+  body: string;
+  createdAt: string;
+  description: string;
+  favorited: boolean;
+  favoritesCount: 0;
+  slug: string;
+  tagList: string[];
+  title: string;
+  updatedAt: string;
+  //to do: author interface
+}
